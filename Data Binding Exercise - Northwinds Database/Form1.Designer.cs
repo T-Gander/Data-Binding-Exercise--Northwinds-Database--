f161028d3748d64f4fa4160cc34199dfe6fd8806
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-05-14 14:51:29 -0600

    Add project files.

---
+++ Data Binding Exercise - Northwinds Database/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Data_Binding_Exercise___Northwinds_Database
+{
+    partial class frmNorthwind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtContactTitle = new System.Windows.Forms.TextBox();
+            this.btnFirstRecord = new System.Windows.Forms.Button();
+            this.btnPreviousRecord = new System.Windows.Forms.Button();
+            this.btnNextRecord = new System.Windows.Forms.Button();
+            this.btnLastRecord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contact Title";
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(98, 12);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(224, 20);
+            this.txtCustomerID.TabIndex = 4;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(98, 38);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(224, 20);
+            this.txtCompanyName.TabIndex = 5;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(98, 90);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(224, 20);
+            this.txtContactName.TabIndex = 6;
+            // 
+            // txtContactTitle
+            // 
+            this.txtContactTitle.Location = new System.Drawing.Point(98, 64);
+            this.txtContactTitle.Name = "txtContactTitle";
+            this.txtContactTitle.Size = new System.Drawing.Size(224, 20);
+            this.txtContactTitle.TabIndex = 7;
+            // 
+            // btnFirstRecord
+            // 
+            this.btnFirstRecord.Location = new System.Drawing.Point(17, 125);
+            this.btnFirstRecord.Name = "btnFirstRecord";
+            this.btnFirstRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnFirstRecord.TabIndex = 8;
+            this.btnFirstRecord.Text = "First Record";
+            this.btnFirstRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnPreviousRecord
+            // 
+            this.btnPreviousRecord.Location = new System.Drawing.Point(98, 125);
+            this.btnPreviousRecord.Name = "btnPreviousRecord";
+            this.btnPreviousRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnPreviousRecord.TabIndex = 9;
+            this.btnPreviousRecord.Text = "<";
+            this.btnPreviousRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnNextRecord
+            // 
+            this.btnNextRecord.Location = new System.Drawing.Point(179, 125);
+            this.btnNextRecord.Name = "btnNextRecord";
+            this.btnNextRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnNextRecord.TabIndex = 10;
+            this.btnNextRecord.Text = ">";
+            this.btnNextRecord.UseVisualStyleBackColor = true;
+            // 
+            // btnLastRecord
+            // 
+            this.btnLastRecord.Location = new System.Drawing.Point(260, 125);
+            this.btnLastRecord.Name = "btnLastRecord";
+            this.btnLastRecord.Size = new System.Drawing.Size(75, 23);
+            this.btnLastRecord.TabIndex = 11;
+            this.btnLastRecord.Text = "Last Record";
+            this.btnLastRecord.UseVisualStyleBackColor = true;
+            // 
+            // frmNorthwind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 159);
+            this.Controls.Add(this.btnLastRecord);
+            this.Controls.Add(this.btnNextRecord);
+            this.Controls.Add(this.btnPreviousRecord);
+            this.Controls.Add(this.btnFirstRecord);
+            this.Controls.Add(this.txtContactTitle);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmNorthwind";
+            this.Text = "Northwind Database Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtContactTitle;
+        private System.Windows.Forms.Button btnFirstRecord;
+        private System.Windows.Forms.Button btnPreviousRecord;
+        private System.Windows.Forms.Button btnNextRecord;
+        private System.Windows.Forms.Button btnLastRecord;
+    }
+}
+
